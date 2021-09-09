8305d3a76919660aded71ef843f7c968bfafcd76
gardemanuelsen <garema@student.kristiania.no>
2021-09-09 14:26:22 +0200

    refactor intRoman

--- src/test/java/no/kristiania/RomanNumeralsTest.java
+++ src/test/java/no/kristiania/RomanNumeralsTest.java
@@ -31,15 +31,15 @@ public class RomanNumeralsTest {
 
 
 
-    public static String intToRoman(int num) {
+    public static String intToRoman(int number) {
         String[] thousands = new String[]{"", "M", "MM", "MMM"};
         String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
         String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         String[] units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        return thousands[num / 1000] +
-                hundreds[(num % 1000) /100] +
-                tens[(num % 100) / 10] +
-                units[num % 10];
+        return thousands[number / 1000] +
+                hundreds[(number % 1000) /100] +
+                tens[(number % 100) / 10] +
+                units[number % 10];
     }
 
     public static void main(String[] args) {
