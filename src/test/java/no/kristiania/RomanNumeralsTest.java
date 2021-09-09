a69f3d8b5cc2811875825ce4e276e3082b1d36f0
gardemanuelsen <garema@student.kristiania.no>
2021-09-09 10:12:42 +0200

    Adding intToRoman method, main method and shouldConvertToOne test

--- src/test/java/no/kristiania/RomanNumeralsTest.java
+++ src/test/java/no/kristiania/RomanNumeralsTest.java
@@ -2,12 +2,32 @@ package no.kristiania;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RomanNumeralsTest {
 
     @Test
-    void dummyTest() {
-        assertEquals(25,5*5);
+    void shouldConvertToOne() {
+        assertEquals("I", RomanNumeralsTest.intToRoman(1));
+    }
+
+    public static String intToRoman(int num) {
+        String[] thousands = new String[]{"", "M", "MM", "MMM"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return thousands[num / 1000] +
+                hundreds[(num % 1000) /100] +
+                tens[(num % 100) / 10] +
+                units[num % 10];
+
+    }
+
+    public static void main(String[] args) {
+        Scanner readData = new Scanner(System.in);
+        System.out.println("Skriv inn tall her:");
+        System.out.println("Romertallet er: " + intToRoman(readData.nextInt()));
     }
 }
