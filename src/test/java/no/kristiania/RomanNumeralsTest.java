ac221072db690b96b303d8538c60bc59cf12da25
gardemanuelsen <garema@student.kristiania.no>
2021-09-09 10:08:55 +0200

    Trying with dummyTest to check if Maven is working

--- src/test/java/no/kristiania/RomanNumeralsTest.java
+++ src/test/java/no/kristiania/RomanNumeralsTest.java
@@ -1,9 +1,13 @@
 package no.kristiania;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RomanNumeralsTest {
 
-
-
-
-
+    @Test
+    void dummyTest() {
+        assertEquals(25,5*5);
+    }
 }
