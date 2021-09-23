acfeee4efc01ca6a0ad3d34861b9f77386c85e14
gardemanuelsen <garema@student.kristiania.no>
2021-09-23 11:06:37 +0200

    fix

--- src/test/java/no/kristiania/RomanNumeralsTest.java
+++ src/test/java/no/kristiania/RomanNumeralsTest.java
@@ -2,30 +2,28 @@ package no.kristiania;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RomanNumeralsTest {
 
   @Test
   public void shouldConvertToOne(){
-      assertEquals("I", main.intToRoman(1));
+      assertEquals("I", Main.intToRoman(1));
   }
 
     @Test
     public void shouldConvertToTen(){
-        assertEquals("X", main.intToRoman(10));
+        assertEquals("X", Main.intToRoman(10));
     }
 
     @Test
     public void shouldConvertToHundred(){
-      assertEquals("C", main.intToRoman(100));
+      assertEquals("C", Main.intToRoman(100));
     }
 
     @Test
     public void shouldConvertToThousands(){
-        assertEquals("M", main.intToRoman(1000));
+        assertEquals("M", Main.intToRoman(1000));
     }
 
 }
